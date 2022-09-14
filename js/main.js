a3f1cce8eaa7a261f239896b4c3aeab2ac0335b8
Joan Cortes <js.199911@gmail.com>
2022-09-14 10:36:21 -0500

    Versión desktop - terminada

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ inputs.forEach((input) => {
 const textsBold = document.querySelectorAll(".txt-purple");
 const textsHidden = document.querySelectorAll(".hidden");
 const textPrice = document.querySelectorAll(".item-voucher-detail.fw-700");
+const textsSubtitleBlod = document.querySelectorAll(
+  ".item-voucher-detail:nth-child(odd):not(.item-voucher-detail.fw-700)"
+);
 
 const mediumDp = matchMedia("(min-width: 768px)");
 const changeSize = (mql) => {
@@ -25,6 +28,9 @@ const changeSize = (mql) => {
     textPrice.forEach((txt) => {
       txt.classList.add("h4");
     });
+    textsSubtitleBlod.forEach((txt) => {
+      txt.classList.add("fw-700");
+    })
     textsBold[1].classList.add("fw-700");
     textsBold[2].classList.add("fw-700");
     textsBold[1].children[0].style.display = "none";
@@ -37,7 +43,7 @@ const changeSize = (mql) => {
       txt.classList.add("hidden");
     });
     textPrice.forEach((txt) => {
-      txt.classList.add("h4");
+      txt.classList.remove("h4");
     });
     textsBold[1].classList.remove("fw-700");
     textsBold[2].classList.remove("fw-700");
